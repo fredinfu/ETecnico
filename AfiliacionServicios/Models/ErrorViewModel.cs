9bcd3c61c58b453ddb067d5bb8add58095a3d324
fredinfu <fredinfu@live.com>
2023-08-08 19:53:18 -0600

    first commit

---
+++ AfiliacionServicios/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AfiliacionServicios.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
